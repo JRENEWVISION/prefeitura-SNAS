e9bed3ad6222ab3ac8c5739017a6bd6c08903be8
JRE NewVision <129543787+JRENEWVISION@users.noreply.github.com>
2023-04-24 16:10:02 -0300

    .

---
+++ app/src/main/java/com/example/snas/cadastrodasfamilias.java
@@ -0,0 +1,13 @@
+package com.example.snas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cadastrodasfamilias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrodasfamilias);
+    }
+}
